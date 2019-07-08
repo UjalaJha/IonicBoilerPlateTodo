c111cabf38d1a144f61a9c3ff591e453b88e040f
UjalaJha <jhaujala3@gmail.com>
2019-07-08 20:32:32 +0530

    Todo Working

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'home', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
+  { path: 'details', loadChildren: './details/details.module#DetailsPageModule' },
+
 ];
 @NgModule({
   imports: [
