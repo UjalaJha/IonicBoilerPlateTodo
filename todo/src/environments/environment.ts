228c3d905ee038cc80c3586307cebf7b4b26fe9d
UjalaJha <jhaujala3@gmail.com>
2019-07-08 18:30:07 +0530

    Provider To connect with firebase

--- todo/src/environments/environment.ts
+++ todo/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA7ygIKMSiqRYC0BEFFGefvakfB_1HlC3E",
+    authDomain: "todo-661d2.firebaseapp.com",
+    databaseURL: "https://todo-661d2.firebaseio.com",
+    projectId: "todo-661d2",
+    storageBucket: "",
+    messagingSenderId: "950957444263",
+    appId: "1:950957444263:web:cef88ab01652d32e"
+  }
 };
 
 /*
